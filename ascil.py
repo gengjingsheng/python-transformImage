47df44ae3b1d7ad47b4322e93ed31c2b7bfd1dcd
gengjingsheng <389952684@qq.com>
2021-02-08 14:50:04 +0800

    图片转字符。

---
+++ ascil.py
@@ -0,0 +1,47 @@
+from PIL import Image
+
+#要处理的图片名
+IMG = 'aa.jpg'
+#宽像素点
+WIDTH = 500
+#高像素点
+HEIGHT = 700
+#输出路径
+OUTPUT = None
+
+ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+
+# 将256灰度映射到70个字符上
+def get_char(r, g, b, alpha=256):
+    if alpha == 0:
+        return ' '
+    length = len(ascii_char)
+    gray = int(0.2126 * r + 0.7152 * g + 0.0722 * b)
+
+    unit = (256.0 + 1) / length
+    return ascii_char[int(gray / unit)]
+
+if __name__ == '__main__':
+
+    #打开图片并重置大小
+    im = Image.open(IMG)
+    im = im.resize((WIDTH, HEIGHT), Image.NEAREST)
+    #显示图片
+    im.show()
+
+    txt = ''
+
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            txt += get_char(*im.getpixel((j, i)))
+        txt += '\n'
+
+    print(txt)
+
+    # 字符画输出到文件
+    if OUTPUT:
+        with open(OUTPUT, 'w') as f:
+            f.write(txt)
+    else:
+        with open("output.txt", 'w') as f:
+            f.write(txt)
